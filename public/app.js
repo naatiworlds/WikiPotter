e7ef315b85151ba07c091b0ca26202fcdfc528d3
naatii <jcorber855@gmail.com>
2025-02-07 15:41:56 +0100

    Probando PWA

---
+++ public/app.js
@@ -0,0 +1,2 @@
+// Aquí podrías agregar funcionalidad adicional de tu aplicación.
+console.log('¡Aplicación PWA cargada!');
---
+++ public/service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'mi-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/styles.css',
+  '/app.js',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
